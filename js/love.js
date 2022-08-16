c5e03a42279939d46529d35b0893c01b42d64e07
hedayatullahh <hedayatullah0303@gmail.com>
2022-08-17 01:41:33 +0600

    first commit

---
+++ js/love.js
@@ -0,0 +1,20 @@
+document.getElementById("button-1").addEventListener("click", function () {
+    const loveLettar = document.getElementById("love-lettar");
+    (loveLettar.innerText = "I LOVE YOU"), alert("ভেবে দেখুন কি বলছেন");
+});
+document.getElementById("button-2").addEventListener("click", function () {
+  const loveLettar = document.getElementById("love-lettar");
+  (loveLettar.innerText = "LOVE YOU VERY MUCH"),
+    alert("দেখুন!! ভালোবাসা কোন ছেলেখেলা নয়");
+});
+document.getElementById("button-3").addEventListener("click", function () {
+  const loveLettar = document.getElementById("love-lettar");
+  (loveLettar.innerText = "Why am I not worthy of you?"),
+    alert("সম্মানের সাথে না করার জন্য ধন্যবাদ");
+});
+document.getElementById("button-4").addEventListener("click", function () {
+  const loveLettar = document.getElementById("love-lettar");
+  loveLettar.innerText = "ok, no problem.",
+        alert("অপমান করার শাস্তি কড়ায়-গণ্ডায় বুঝিয়ে দিব");
+    
+});
